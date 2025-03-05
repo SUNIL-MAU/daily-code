a6de980699a4a19408a2524c27c8940d2a3b1edb
xceloresunil <145955146+xceloresunil@users.noreply.github.com>
2025-03-05 12:48:24 +0530

    accordian done with data mapping

---
+++ matrix-data.js
@@ -0,0 +1,43 @@
+const matrixData = [
+  {
+    "CYBER COMMS CHANNELS MATRIX": "HSBC Central Workplace Directory",
+    Channel: "x",
+    Link: {
+      text: "Cybersecurity (sharepoint.com)",
+      hyperlink:
+        "https://www.istockphoto.com/photo/bridge-over-sea-against-clear-sky-during-sunset-gm2169699402-589672056?utm_source=pixabay&amp;utm_medium=affiliate&amp;utm_campaign=sponsored_image&amp;utm_content=srp_topbanner_media&amp;utm_term=link",
+    },
+    "How to engage": "Contact Cyber\nComms/Engagement",
+    Frequency: "Available all time",
+    "Target Audience": "ALL HSBC",
+    "Actual Audience (can access)": "AII HSBC",
+    "Content type": "General HSBC org. Design information + directory",
+    "Content format": "HSBC GIC standard",
+    "Owner(s) / restrictions": "Cyber Comms/Engage ment only",
+    Metrics: "Adherance to standard",
+    "Cyber Content Framework": "NA",
+  },
+  {
+    "CYBER COMMS CHANNELS MATRIX": "Cyber Central",
+    Channel: "Subpages per topic",
+    Link: {
+      text: "https://social.global.hsbc/cybertechnology/hor",
+      hyperlink: "https://social.global.hsbc/cybertechnology/hor",
+    },
+    "How to engage":
+      "Cyber D&UX (via Cyber Central) or Cyber Comms/Engagement (Directly)",
+    Frequency: "Available all time",
+    "Target Audience": "ALL HSBC",
+    "Actual Audience (can access)": "AII HSBC",
+    "Content type":
+      "Provide all colleagues seamless access to cybersecurity awareness guidance and resources, inform audience about cybersecurity offerings, strategy and business objectives, and support ,cybersecurity colleaues with useful resources.",
+    "Content format": "Cyber Central standard",
+    "Owner(s) / restrictions": "Cyber D&UX & Cyber Comms/Engage ment only",
+    Metrics: "Activity Rates",
+    "Cyber Content Framework": {
+      text: "Cyber Central - Quick guide",
+      hyperlink:
+        "https://www.istockphoto.com/photo/bandra-worli-sea-link-gm531864277-55701756?utm_source=pixabay&amp;utm_medium=affiliate&amp;utm_campaign=sponsored_image&amp;utm_content=srp_topbanner_media&amp;utm_term=link",
+    },
+  },
+];
